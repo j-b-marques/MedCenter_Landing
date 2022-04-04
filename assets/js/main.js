da6b612945010bb65bd179ac13e98c939cd01ff7
João Marquês <99401638+j-b-marques@users.noreply.github.com>
2022-04-04 21:16:49 +0100

    Comit

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,46 @@ const submitBtn = document.querySelector('.btn__form');
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault(); //* for demo purposes
 });
+
+//*************SCROLL REVEALL **********/
+
+const allRevealsHeader = [
+  'nav',
+  '.header__lead__wrapper',
+  '.header__bottom__wrapper',
+  '.img__header',
+  '.services',
+  '.contact',
+  '.staff',
+  '.blog',
+  '.map',
+];
+
+for (i = 0; i < allRevealsHeader.length; i++) {
+  ScrollReveal().reveal(allRevealsHeader[i], {
+    delay: 400,
+    easing: 'ease-in',
+    duration: 600,
+  });
+}
+//***********MATRIX CARD*********/
+$(document).ready(function () {
+  $('.square').mousemove(function (e) {
+    var offset = $(this).offset();
+    var relX = e.pageX - offset.left;
+    var relY = e.pageY - offset.top;
+    var offsetMinX = $(this).width();
+    var offsetMinY = $(this).height();
+    var currentX = (relX += offsetMinX * -0.5);
+    var currentY = (relY += offsetMinY * -0.5);
+    var newX = currentX / 500000;
+    var newY = currentY / 1000000;
+    $(this).css(
+      'transform',
+      'matrix3d(1.025,0,0,' + -newX + ',0,1.025,0,' + -newY + ',0,0,1,0,0,0,0,1)'
+    );
+  });
+  $('.square').mouseout(function () {
+    $(this).css('transform', 'matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1)');
+  });
+});
